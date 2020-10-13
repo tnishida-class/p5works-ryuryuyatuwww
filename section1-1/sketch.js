207e33ac35f8c1168c62c96561028dc822d1841e
ryuryuyatuwww <72425065+ryuryuyatuwww@users.noreply.github.com>
2020-10-13 12:22:03 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -5,8 +5,8 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
   triangle(12,120,116,120,116,10);
+  fill(178, 100, 162);
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
   fill(255);
