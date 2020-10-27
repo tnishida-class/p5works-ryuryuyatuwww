305afbbc1fa7a56e5f96229c3ca6ca37be25dda5
ryuryuyatuwww <72425065+ryuryuyatuwww@users.noreply.github.com>
2020-10-27 11:54:12 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -9,9 +9,25 @@ function setup() {
     fill(225);
     }
  else{
-        fill(0);
-      }
-  rect(size*1,size*2,size,size);
+        fill(128);
+ }
+  rect(i*10,j*10,10,10);
     }
+  for(let i=0;i<8;i++){
+    for(let j =0;j<3;j++){
+      if((i+j)%2==1){
+        fill(255,0,0);
+        ellipse(i*10+5,j*10+5,10,10)
+ for(let i=0;i<8;i++){
+   for(let j=0;6<j<8;j++){
+     if((i+j)%2==1){
+       fill(255);
+       ellipse(i*10+5,j*10+5,10,10)
+     }
+   }
+ }
+      }
+    }
+  }
   }
 }
