cb084aab6c183f3469f71ed4f986a7e259c696e1
ryuryuyatuwww <72425065+ryuryuyatuwww@users.noreply.github.com>
2021-10-26 00:22:24 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,13 @@
 // 小手調べ
 function setup(){
+  noFill();
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+  let x = (i+1)* 10;
+  ellipse(50,50,x);
+  if (i<4) {
+  stroke(0,0,255);
 }
+  else{
+  stroke(255,0,0)}
+  }
+  }
